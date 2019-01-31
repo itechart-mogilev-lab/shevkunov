561fc5ac6d721e78a989e146ab998cabc0184a34
Шевкунов Вадим Вячеславович <shevkunov@himvol.by>
2019-01-31 14:57:23 +0300

    Changes in script. Add the eventlistner on event resize for view optimtization of menu items

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ function myFunction() {
     x[0].className = "main-navigation__row";
   }
 }
+(function() {
+
+  window.addEventListener("resize", resizeThrottler, false);
+
+  var resizeTimeout;
+  function resizeThrottler() {
+    // ignore resize events as long as an actualResizeHandler execution is in the queue
+    if ( !resizeTimeout ) {
+      resizeTimeout = setTimeout(function() {
+        resizeTimeout = null;
+        actualResizeHandler();
+       }, 66);
+    }
+  }
+
+  function actualResizeHandler(){
+    var x = document.getElementsByClassName("main-navigation__row");
+      if (x[0].className === "main-navigation__row main-navigation__row__responsive") {
+        x[0].className = "main-navigation__row";  
+      } 
+  }
+}());
