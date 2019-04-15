7283e4f75f6e78e6b52440f330e09c5b452cb9f8
Шевкунов Вадим Вячеславович <shevkunov@himvol.by>
2019-04-15 16:24:44 +0300

    Add services inputs to register compan component(not done)

--- frontend/src/helpers/enum.js
+++ frontend/src/helpers/enum.js
@@ -1,33 +1,33 @@
 export const sortType = [
-  {
-    value: "",
-    name: ""
-  },
-  { value: "price_asc", name: "Price ascending" },
-  { value: "price_desc", name: "Price descending" },
-  { value: "rating_asc", name: "Rating ascending" },
-  { value: "rating_desc", name: "Rating descending" },
-  { value: "popularity_asc", name: "Popularity ascending" },
-  { value: "popularity_desc", name: "Popularity descending" }
+	{
+		value: "",
+		name: ""
+	},
+	{ value: "price_asc", name: "Price ascending" },
+	{ value: "price_desc", name: "Price descending" },
+	{ value: "rating_asc", name: "Rating ascending" },
+	{ value: "rating_desc", name: "Rating descending" },
+	{ value: "popularity_asc", name: "Popularity ascending" },
+	{ value: "popularity_desc", name: "Popularity descending" }
 ];
 
 export const selectCity = [
-  { value: "", name: "Все города" },
-  { value: "Могилев", name: "Могилев" },
-  { value: "Витебск", name: "Витебск" },
-  { value: "Гродно", name: "Гродно" },
-  { value: "Минcк", name: "Минcк" },
-  { value: "Гомель", name: "Гомель" },
-  { value: "Брест", name: "Брест" }
+	{ value: "", name: "Все города" },
+	{ value: "Могилев", name: "Могилев" },
+	{ value: "Витебск", name: "Витебск" },
+	{ value: "Гродно", name: "Гродно" },
+	{ value: "Минcк", name: "Минcк" },
+	{ value: "Гомель", name: "Гомель" },
+	{ value: "Брест", name: "Брест" }
 ];
 
 export const selectService = [
-  { value: "", name: "" },
-  { value: "Standart", name: "Standart" },
-  { value: "Total", name: "Total" },
-  { value: "After_repair", name: "After repair" },
-  { value: "Chemical_for_carpets", name: "Chemical for carpets" },
-  { value: "Office", name: "Office" },
-  { value: "Chemical_for_furniture", name: "Chemical for furniture" },
-  { value: "Pool", name: "Pool" }
+	{ value: "", name: "", key: 0 },
+	{ value: "Standart", name: "Standart", key: 1 },
+	{ value: "Total", name: "Total", key: 2 },
+	{ value: "After_repair", name: "After repair", key: 3 },
+	{ value: "Chemical_for_carpets", name: "Chemical for carpets", key: 4 },
+	{ value: "Office", name: "Office", key: 5 },
+	{ value: "Chemical_for_furniture", name: "Chemical for furniture", key: 6 },
+	{ value: "Pool", name: "Pool", key: 7 }
 ];
